e5f98b53603a6c14a1a4c5b46dc25cfad060ed9a
Toughson <brighttoughson@gmail.com>
2026-06-16 17:19:45 +0000

    Fix splash screen filename typo

--- src/App.tsx
+++ src/App.tsx
@@ -57,8 +57,8 @@ export default function App() {
                 >
                   <img 
                     src={splashImg} 
-                    alt="myTroski Go Logo" 
-                    style={{ width: 140, height: 140, borderRadius: 30, boxShadow: '0 10px 30px rgba(251, 191, 36, 0.2)' }} 
+                    alt="myTroski Go Splash" 
+                    style={{ width: 120, height: 120, borderRadius: 30, boxShadow: '0 10px 30px rgba(251, 191, 36, 0.2)' }} 
                   />
                 </motion.div>
                 
